3d2811b02a9239fbc3f4f1aed08252979c731e05
katekyungmi <katekyungmi@gmail.com>
2026-08-07 21:00:02 +0900

    Initialize NUDGEON_CONFIG with Google Sheets ID
    Add configuration for Google Sheets ID.

---
+++ config.js
@@ -0,0 +1,8 @@
+/*
+  Google Sheets 주소에서 /d/ 와 /edit 사이의 값을 아래에 붙여 넣으세요.
+  예: https://docs.google.com/spreadsheets/d/1AbCdEf.../edit
+      → GOOGLE_SHEET_ID: '1AbCdEf...'
+*/
+window.NUDGEON_CONFIG = {
+  GOOGLE_SHEET_ID: '1XZSwhGSeRjMkk7KEBkGv_7_lOJgyJmyP'
+};
