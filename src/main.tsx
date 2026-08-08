f843312744ce7f799ad8409216c10e7f6613c875
hojing0612 <hojing0612@yonsei.ac.kr>
2026-08-08 16:31:00 +0900

    integrate NudgeOn journey with social rehearsal

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const isRehearsal = window.location.pathname.replace(/\/$/, '') === '/rehearsal';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isRehearsal ? (
+      <App />
+    ) : (
+      <iframe
+        title="NudgeOn 실행 지원 여정"
+        src="/home.html"
+        style={{ width: '100%', height: '100dvh', border: 0, display: 'block' }}
+      />
+    )}
   </StrictMode>
 );
